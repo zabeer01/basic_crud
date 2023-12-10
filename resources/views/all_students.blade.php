4fb3a0840909c74c15df67e9ca81c1b3b55b0c36
Jabir Ibne <ibnejabir001@gmail.com>
2023-12-10 14:11:19 +0600

    Bootbox added for pop up edit

--- resources/views/all_students.blade.php
+++ resources/views/all_students.blade.php
@@ -1,83 +1,90 @@
 @extends('master')
 
 @section('content')
-   <div class="p-5">
-    <table class="table">
-        <thead class="thead-light">
-            <tr>
-                <th scope="col">SI</th>
-                <th scope="col">name</th>
-                <th scope="col">img</th>
-                <th scope="col">action</th>
-            </tr>
-        </thead>
-        <tbody>
-            @php
-                $i = 1;
-            @endphp
-            @foreach ($students as $std)
+    <div class="p-5">
+        <table class="table">
+            <thead class="thead-light">
                 <tr>
-                    <th scope="row">{{ $i++ }}</th>
-
-                    <td>{{ $std->name }}</td>
-                    <td>
-                        <!-- Display the student image -->
-                        <img src="{{ asset($std->image) }}" alt="Student Image" style="max-width: 100px; max-height: 100px;">
-                    </td>
-                    <td>
-
-                        <a href="{{ route('edit_students', ['id' => $std->id]) }}" class="btn btn-dark">edit</a>
-                       <button type="button" class="btn btn-danger" id="test">Danger</button>
-                    </td>
-
-
+                    <th scope="col">SI</th>
+                    <th scope="col">name</th>
+                    <th scope="col">img</th>
+                    <th scope="col">action</th>
                 </tr>
-            @endforeach
+            </thead>
+            <tbody>
+                @php
+                    $i = 1;
+                @endphp
+                @foreach ($students as $std)
+                    <tr>
+                        <th scope="row">{{ $i++ }}</th>
+
+                        <td>{{ $std->name }}</td>
+                        <td>
+                            <!-- Display the student image -->
+                            <img src="{{ asset($std->image) }}" alt="Student Image"
+                                style="max-width: 100px; max-height: 100px;">
+                        </td>
+                        <td>
+
+                            <a class="btn btn-dark edit-btn" id="test" data-id="{{ $std->id }}">
+
+                                {{-- href="{{ route('edit_students', ['id' => $std->id]) }}" --}}
+
+                                Edit
+
+                            </a>
+
+                        </td>
 
 
-    </table>
-   </div>
+                    </tr>
+                @endforeach
+
+
+        </table>
+    </div>
 @endsection
 
 
 {{-- custom js --}}
 @section('script')
-<script>
-    $(document).ready(function () {
-        //modal show 
-        $(document).on('click','#test', function () {
-            let dialog = bootbox.dialog({
-                                title: 'A custom dialog with buttons and callbacks',
-                                message: "<p>This dialog has buttons. Each button has it's own callback
-                                function.</p>",
-                                size: 'large',
-                                buttons: {
-                                cancel: {
-                                label: "I'm a cancel button!",
-                                className: 'btn-danger',
-                                callback: function(){
-                                console.log('Custom cancel clicked');
-                                }
-                                },
-                                noclose: {
-                                label: "I don't close the modal!",
-                                className: 'btn-warning',
-                                callback: function() {
-                                console.log('Custom button clicked');
-                                return false;
-                                }
-                                },
-                                ok: {
-                                label: "I'm an OK button!",
-                                className: 'btn-info',
-                                callback: function() {
-                                console.log('Custom OK clicked');
-                                }
-                                }
-                                }
-                                });
-            
+    <script>
+        $(document).ready(function() {
+            // Handle click event on the Edit button
+            $(document).on('click', '#test', function(e) {
+                e.preventDefault();
+
+                // Retrieve the student ID from the data-id attribute
+                let studentId = $(this).data('id');
+
+                // Construct the edit endpoint URL using the student ID
+                let editEndpoint = `{{ route('edit_students', ['id' => 'studentIdPlaceholder']) }}`.replace(
+                    'studentIdPlaceholder', studentId);
+
+                // Open a Bootbox modal for editing and load form content using AJAX
+                bootbox.dialog({
+                    title: 'Edit Student',
+                    message: '<div class="bootbox-body"><form id="editStudentForm">Loading form...</form></div>',
+                    size: 'large',
+                    buttons: {},
+                    show: true
+                });
+
+                // Fetch the edit form content using AJAX
+                $.ajax({
+                    url: editEndpoint,
+                    type: 'GET',
+                    success: function(data) {
+                        // Replace the message content with the fetched edit form
+                        $('.bootbox-body').html(data);
+                    },
+                    error: function() {
+                        // Handle errors if the form content cannot be fetched
+                        $('.bootbox-body').html('Failed to load form.');
+                    }
+                });
+            });
         });
-    });
-</script>
+    </script>
 @endsection
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -36,9 +36,11 @@
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-<script>
+    {{-- jquery --}}
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    {{-- bootbox --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.5.2/bootbox.min.js" integrity="sha512-RdSPYh1WA6BF0RhpisYJVYkOyTzK4HwofJ3Q7ivt/jkpW6Vc8AurL1R+4AUcvn9IwEKAPm/fk7qFZW3OuiUDeg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-</script>
+
     
     @yield('script')
 </body>
