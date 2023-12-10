5bea61bf697e83905d9fa17ca7cf04b7732f49da
Jabir Ibne <ibnejabir001@gmail.com>
2023-12-10 10:33:50 +0600

    bootbox

--- resources/views/all_students.blade.php
+++ resources/views/all_students.blade.php
@@ -27,7 +27,9 @@
                     <td>
 
                         <a href="{{ route('edit_students', ['id' => $std->id]) }}" class="btn btn-dark">edit</a>
+                       <button type="button" class="btn btn-danger" id="test">Danger</button>
                     </td>
+
 
                 </tr>
             @endforeach
@@ -35,4 +37,47 @@
 
     </table>
    </div>
+@endsection
+
+
+{{-- custom js --}}
+@section('script')
+<script>
+    $(document).ready(function () {
+        //modal show 
+        $(document).on('click','#test', function () {
+            let dialog = bootbox.dialog({
+                                title: 'A custom dialog with buttons and callbacks',
+                                message: "<p>This dialog has buttons. Each button has it's own callback
+                                function.</p>",
+                                size: 'large',
+                                buttons: {
+                                cancel: {
+                                label: "I'm a cancel button!",
+                                className: 'btn-danger',
+                                callback: function(){
+                                console.log('Custom cancel clicked');
+                                }
+                                },
+                                noclose: {
+                                label: "I don't close the modal!",
+                                className: 'btn-warning',
+                                callback: function() {
+                                console.log('Custom button clicked');
+                                return false;
+                                }
+                                },
+                                ok: {
+                                label: "I'm an OK button!",
+                                className: 'btn-info',
+                                callback: function() {
+                                console.log('Custom OK clicked');
+                                }
+                                }
+                                }
+                                });
+            
+        });
+    });
+</script>
 @endsection
